c61a1ac1ac8f7fb4b94202c93b642ca3d2d749a9
T4ti4n4 <rooney2001.TBG@gmail.com>
2024-08-28 11:04:36 -0500

    Cambios
    Se hacen cambios

---
+++ src/SimpleStudentCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SimpleStudentCard(props) {
+  return (
+    <div>
+      <h2>El estudiante es: {props.name}</h2>
+      <9>El estudiante es: {props.name2}</9>
+    </div>
+  );
+}
+// Se hace la propiedad llamada name
+
+export default SimpleStudentCard ;
